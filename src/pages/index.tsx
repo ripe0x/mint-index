be58b76d911efce874138f2498140cf1a7a1462e
ripe0x <unripeengine@gmail.com>
2024-11-18 11:34:06 -0500

    add og metadata

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,21 @@ export default function Home() {
           name="description"
           content="explore the latest creations on mint protocol"
         />
+        <meta property="og:title" content="NetworkedNodes.art" />
+        <meta
+          property="og:description"
+          content="explore the latest creations on mint protocol"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://networkednodes.art" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@ripe0x" />
+        <meta name="twitter:title" content="NetworkedNodes.art" />
+        <meta
+          name="twitter:description"
+          content="explore the latest creations on mint protocol"
+        />
+
         <link rel="icon" href="/favicon.svg" />
       </Head>
       <div className="flex flex-col row-start-2 items-center sm:items-start">
