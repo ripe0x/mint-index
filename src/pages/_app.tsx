60341d1ecc9553d3e20638b0bba21db6ad4a37ef
ripe0x <unripeengine@gmail.com>
2024-12-16 11:45:21 -0500

    add fallback providers in config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ import {
   midnightTheme,
 } from "@rainbow-me/rainbowkit";
 import { mainnet, sepolia } from "viem/chains";
-import { WagmiProvider } from "wagmi";
+import { fallback, http, WagmiProvider } from "wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { IBM_Plex_Mono } from "next/font/google";
 import Layout from "@/components/Layout";
@@ -16,10 +16,37 @@ const config = getDefaultConfig({
   appName: "My RainbowKit App",
   projectId: "YOUR_PROJECT_ID",
   chains: [process.env.NEXT_PUBLIC_IS_TESTNET ? sepolia : mainnet],
-  ssr: true, // If your dApp uses server side rendering (SSR)
+  ssr: true,
+  transports: {
+    [mainnet.id]: fallback([
+      http(
+        `https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`
+      ),
+      http(
+        `https://mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_API_KEY}`
+      ),
+      http(),
+    ]),
+    [sepolia.id]: fallback([
+      http(
+        `https://eth-sepolia.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`
+      ),
+      http(
+        `https://sepolia.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_API_KEY}`
+      ),
+      http(),
+    ]),
+  },
 });
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 3,
+      retryDelay: (attemptIndex) => Math.min(1000 * 2 ** attemptIndex, 30000),
+    },
+  },
+});
 const ibmPlexMono = IBM_Plex_Mono({
   subsets: ["latin"],
   weight: ["300", "400"],
