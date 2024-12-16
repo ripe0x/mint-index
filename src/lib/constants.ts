a73b7a681c40e747b835936c4d65497477e45012
ripe0x <unripeengine@gmail.com>
2024-12-16 12:14:20 -0500

    update env vars

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,13 +1,13 @@
 export const EXTERNAL_MINT_BASE_URL = "https://networked.art";
 let factoryAddress =
   "0xd717Fe677072807057B03705227EC3E3b467b670" as `0x${string}`;
-let rpcUrl = `https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API}`; // Replace with your Alchemy/Infura key
+let rpcUrl = `https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`; // Replace with your Alchemy/Infura key
 let factoryDeploymentBlock = 21167599;
 
 if (process.env.NEXT_PUBLIC_IS_TESTNET === "true") {
   factoryAddress =
     "0x750C5a6CFD40C9CaA48C31D87AC2a26101Acd517" as `0x${string}`;
-  rpcUrl = `https://eth-sepolia.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API}`;
+  rpcUrl = `https://eth-sepolia.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`;
   factoryDeploymentBlock = 7057962;
 }
 
