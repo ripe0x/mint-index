9e81ce9b339110de0af98530ff39761d9a2b348c
ripe0x <unripeengine@gmail.com>
2025-02-26 13:47:40 -0500

    fix redirects

--- next.config.ts
+++ next.config.ts
@@ -5,32 +5,6 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["ipfs.io"],
   },
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "mint-index.netlify.app",
-          },
-        ],
-        destination: "https://nodeworks.art/:path*",
-        permanent: true,
-      },
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "nodeworks.art",
-          },
-        ],
-        destination: "https://nodeworks.art/:path*",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
